13b957194892e79779ba367a7b181a1f7a39ee29
Mdizzath7373 <mohamedizzath7373@gmail.com>
2025-07-25 11:24:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const app express();
-const path require('path');
+const app = express();
+const path = require('path'); 
 
 app.get('/', (req, res) => {
- res.sendFile(path.join(_dirname+'/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html')); 
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(_dirname+'/about.html'));
-
- });
+    res.sendFile(path.join(__dirname, 'about.html')); 
+});
 
 app.listen(8081, () => {
     console.log('Listening on port 8081');
-}):
+});  
